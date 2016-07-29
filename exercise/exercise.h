a7e95cfd8bb1a9b708651ba85617142e38d92bcc
baijie <baijie0918@163.com>
2016-07-29 16:50:18 +0800

    add StrBlob

--- exercise/exercise.h
+++ exercise/exercise.h
@@ -71,4 +71,24 @@ public:
     vector<string> phones;
 };
 
+class StrBlob
+{
+public:
+    typedef vector<string>::size_type size_type;
+    StrBlob();
+    StrBlob(initializer_list<string> il);
+    size_type size() const {return data->size();}
+    bool empty() const {return data->empty();}
+    void push_back(const string &t) {data->push_back(t);}
+    void pop_back();
+    string &front();
+    string &back();
+private:
+    shared_ptr<vector<string> > data;
+    void check(size_type i, const string &msg) const;
+};
+
+StrBlob::StrBlob():data(make_shared<vector<string> >()) {}
+StrBlob::StrBlob(initializer_list<string> il):data(make_shared<vector<string> >(il)) {}
+
 #endif // EXERCISE_H
